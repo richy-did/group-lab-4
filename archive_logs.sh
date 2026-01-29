bac75210830a40c1df98b206a86a34fc3e598df1
orionkabeza <o.kabeza@alustudent.com>
2026-01-29 03:17:39 -0800

    Task_1:Menu_update

--- archive_logs.sh
+++ archive_logs.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/bash
 
 LOG_DIR="hospital_data/active_logs"
+REPORT="reports/analysis_report.txt"
 
-echo "Select log to archive:"
+mkdir -p reports
+
+echo "Select log file to analyze:"
 echo "1) Heart Rate"
 echo "2) Temperature"
 echo "3) Water Usage"
@@ -10,25 +13,20 @@ read -p "Enter choice (1-3): " choice
 
 case $choice in
   1)
-    FILE="$LOG_DIR/heart_rate_log.log"
-    ARCHIVE_DIR="hospital_data/heart_data_archive"
-    NAME="heart_rate"
-    DISPLAY_NAME="heart_rate.log"
+    FILE="$LOG_DIR/heart_rate.log"
+    TITLE="Heart Rate Analysis"
     ;;
   2)
-    FILE="$LOG_DIR/temperature_log.log"
-    ARCHIVE_DIR="hospital_data/temp_data_archive"
-    NAME="temperature"
-    DISPLAY_NAME="temperature.log"
+    FILE="$LOG_DIR/temperature.log"
+    TITLE="Temperature Analysis"
     ;;
   3)
-    FILE="$LOG_DIR/water_usage_log.log"
-    ARCHIVE_DIR="hospital_data/water_usage_archive"
-    NAME="water_usage"
-    DISPLAY_NAME="water_usage.log"
+    FILE="$LOG_DIR/water_usage.log"
+    TITLE="Water Usage Analysis"
     ;;
   *)
-    echo "Invalid choice. Please choose among the available options!!"
+    echo "Invalid choice.Please choose among the available options."
     exit 1
     ;;
 esac
+
